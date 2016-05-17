8ea8db0c7f7c6cc465985de1af7f7cad03ad13eb
rcastrogi <castrogi701@hotmail.com>
2016-05-17 15:51:56 -0300

    Muitas atualizações importantes

--- LojaVirtual.UnitTest/UnitTestLojaVirtual.cs
+++ LojaVirtual.UnitTest/UnitTestLojaVirtual.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Web.Mvc;
+using LojaVirtual.Web.Models;
+using LojaVirtual.Web.HtmlHelpers;
 
 namespace LojaVirtual.UnitTest
 {
@@ -7,9 +11,62 @@ namespace LojaVirtual.UnitTest
     public class UnitTestLojaVirtual
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Take()
         {
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var resultado = from num in numeros.Take(5) select num;
+
+            int[] teste = { 5, 4, 1, 3, 9 };
+
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+
 
         }
+
+
+        [TestMethod]
+        public void Skip()
+        {
+
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var resultado = from num in numeros.Take(5).Skip(2) select num;
+
+            int[] teste = { 1, 3, 9 };
+
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+
+
+        }
+
+        [TestMethod]
+        public void TestarSePaginacaofunciona()
+        {
+            //Arrange
+            HtmlHelper html = null;
+
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensPorPagina = 10,
+                ItensTotal = 28
+            };
+
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            //Act
+            MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+
+            //Assert
+            Assert.AreEqual(
+
+                @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+                + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"">2</a>"
+                + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>", resultado.ToString()
+                );
+        }
     }
+
+    
 }
---
+++ LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,43 @@
+﻿using LojaVirtual.Dominio.Entidades;
+using LojaVirtual.Dominio.Repositorio;
+using LojaVirtual.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+
+        private ProdutoRepositorio _repositorio;
+        public int ProdutosPorPagina = 8; 
+
+
+        // GET: Vitrine
+        public ViewResult ListaProdutos(int pagina = 1)
+        {
+
+            _repositorio = new ProdutoRepositorio();
+
+            ProdutosViewModel model = new ProdutosViewModel
+            {
+                Produtos = _repositorio.Produto
+                .OrderBy(p => p.Descricao)
+                .Skip((pagina - 1) * ProdutosPorPagina)
+                .Take(ProdutosPorPagina),
+
+                Paginacao = new Paginacao
+                {
+                    PaginaAtual = pagina,
+                    ItensPorPagina = ProdutosPorPagina,
+                    ItensTotal = _repositorio.Produto.Count()
+                }
+            };
+
+            return View(model);
+        }
+    }
+}
